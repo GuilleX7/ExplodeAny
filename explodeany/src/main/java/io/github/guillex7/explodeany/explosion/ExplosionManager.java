63a0a815a285387aebbe17c01eb853bb9d4e4eb5
GuilleX7 <>
2024-02-25 03:26:16 +0100

    Add multithread safety and support for Folia servers, NO-REF-ISSUE

--- explodeany/src/main/java/io/github/guillex7/explodeany/explosion/ExplosionManager.java
+++ explodeany/src/main/java/io/github/guillex7/explodeany/explosion/ExplosionManager.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -32,6 +33,7 @@ import io.github.guillex7.explodeany.explosion.metadata.ExplosionMetadata;
 
 public class ExplosionManager {
     private static ExplosionManager instance;
+    private static final int CHUNK_RADIUS = 8;
 
     public static final String EXPLOSION_MANAGER_SPAWNED_TAG = "eany-em-spawned";
     public static final String EXPLOSION_MANAGER_EXPLOSION_METADATA_TAG = "eany-em-explosion-metadata";
@@ -115,6 +117,13 @@ public class ExplosionManager {
         final int squaredExplosionRadius = explosionRadius * explosionRadius;
         final World sourceWorld = sourceLocation.getWorld();
         final Location sourceBlockLocation = new Location(sourceWorld, cx, cy, cz);
+
+        if (!Bukkit.isOwnedByCurrentRegion(sourceLocation, explosionRadius / CHUNK_RADIUS)) {
+            ExplodeAny.getInstance().getLogger()
+                    .warning("Explosion at " + sourceLocation
+                            + " was not handled to protect thread safety. Please report this to the plugin author if you see this message often.");
+            return false;
+        }
 
         final EntityBehavioralConfiguration entityBehavioralConfiguration = entityConfiguration
                 .getEntityBehavioralConfiguration();
